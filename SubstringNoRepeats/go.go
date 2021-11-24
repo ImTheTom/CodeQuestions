94deb336ae64ad69a7bf117f9252b71c751f7c14
Tom Bowyer <tom4310@gmail.com>
2021-11-24 16:14:11 +1000

    Added accepted answer

--- SubstringNoRepeats/go.go
+++ SubstringNoRepeats/go.go
@@ -3,12 +3,40 @@ package main
 import "fmt"
 
 const (
-	in  = "abcabcbb"
-	out = 3 // abc
+	in  = "dvdf"
+	out = 3
 )
 
 func lengthOfLongestSubstring(s string) int {
-	return 0
+	longest := 0
+	current := 0
+	longestString := ""
+
+	currentlySeen := make(map[rune]int)
+	for i := 0; i < len(s); i++ {
+		v := rune(s[i])
+
+		if currentlySeen[v] == 1 {
+			currentlySeen = make(map[rune]int)
+			if current > longest {
+				longest = current
+			}
+
+			i = i - len(longestString)
+			longestString = ""
+			current = 0
+		} else {
+			currentlySeen[v] = 1
+			current++
+			longestString += string(v)
+		}
+	}
+
+	if current > longest {
+		longest = current
+	}
+
+	return longest
 }
 
 func main() {
