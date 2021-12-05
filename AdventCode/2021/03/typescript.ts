bafbf0acbe12a801cd5d9ddce20abddacc1df64d
Tom Bowyer <tom4310@gmail.com>
2021-12-05 12:32:34 +1000

    first star

--- AdventCode/2021/03/typescript.ts
+++ AdventCode/2021/03/typescript.ts
@@ -1,6 +1,19 @@
 import fs from 'fs';
 import readline from 'readline';
 
+async function buildFullBinary(input: Array<number>, total: number, mostCommon: boolean): Promise<string> {
+  let result: string = '';
+  for (let i = 0; i < input.length; i++) {
+    const numberOfOnes = input[i];
+    if (numberOfOnes > (total/2)) {
+      result = mostCommon ? result.concat('1') : result.concat('0');
+    } else {
+      result = mostCommon ? result.concat('0') : result.concat('1');
+    }
+  }
+  return result;
+}
+
 async function processLineByLine() {
   const fileStream = fs.createReadStream('input.txt');
 
@@ -9,11 +22,38 @@ async function processLineByLine() {
     crlfDelay: Infinity
   });
 
+  let numberOfOnes = new Array(12);
+  let firstRun = true;
+  let total = 0;
+
   for await (const line of rl) {
-    console.log(line);
+
+    for (let i = 0; i < line.length; i++) {
+      if (firstRun) {
+        numberOfOnes[i] = 0;
+      }
+      if (line[i] === '1') {
+        numberOfOnes[i]++;
+      }
+    }
+
+    firstRun = false;
+    total++;
   }
 
-  return 0;
+  const gammaB = await buildFullBinary(numberOfOnes, total, true);
+  const epsilonB = await buildFullBinary(numberOfOnes, total, false);
+
+  const gamma = parseInt(gammaB, 2)
+  const epsilon = parseInt(epsilonB, 2)
+
+  console.log(gammaB);
+  console.log(epsilonB);
+
+  console.log(gamma);
+  console.log(epsilon);
+
+  return gamma * epsilon;
 }
 
 async function main() {
