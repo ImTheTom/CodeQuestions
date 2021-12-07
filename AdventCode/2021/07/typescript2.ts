85f599ac5e134fb66aa1bca5553432c78a16baf8
Tom Bowyer <tom4310@gmail.com>
2021-12-07 16:26:45 +1000

    Day 7

---
+++ AdventCode/2021/07/typescript2.ts
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import readline from 'readline';
+
+async function process(input: number[]): Promise<number> {
+  const max = Math.max(...input);
+  const min = Math.min(...input);
+  let totalCost = 9223372036854775807;
+  let currentCost = 0;
+  let totalMovement = 0;
+  for (let i = min; i <= max; i++) {
+    for (let j = 0; j < input.length; j++) {
+      totalMovement = Math.abs(i - input[j]);
+      for (let k = totalMovement; k > 0; k--) {
+        currentCost += k;
+      }
+    }
+    if (currentCost < totalCost) {
+      totalCost = currentCost;
+    }
+    currentCost = 0;
+  }
+  return totalCost;
+}
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('input.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  var input;
+
+  for await (const line of rl) {
+    input = line;
+  }
+
+  if (input === undefined) {
+    return 0;
+  }
+
+  var numericalInput = input.split(',').map(Number);
+
+  return await process(numericalInput);
+}
+
+async function main() {
+  console.log("Running code");
+
+  const result = await processLineByLine();
+  console.log(result);
+}
+
+main();
