69de17ca1d1fb89450b9ae37dfa6d7b4605f9541
Tom Bowyer <tom4310@gmail.com>
2021-12-09 07:42:30 +1000

    reset day 8

--- AdventCode/2021/08/typescript2.ts
+++ AdventCode/2021/08/typescript2.ts
@@ -1,6 +1,20 @@
 import fs from 'fs';
 import readline from 'readline';
 
+const uniqueValues = [2,4,3,7];
+
+async function processInputs(inputs: string[][], outputs: string[][]): Promise<number> {
+  let total = 0;
+  for (let i = 0; i < inputs.length; i++) {
+    for (let j = 0; j < outputs[i].length; j++) {
+      if (uniqueValues.includes(outputs[i][j].length)) {
+        total++;
+      }
+    }
+  }
+  return total;
+}
+
 async function processLineByLine() {
   const fileStream = fs.createReadStream('input.txt');
 
@@ -9,11 +23,27 @@ async function processLineByLine() {
     crlfDelay: Infinity
   });
 
+  var lines: string[] = [];
+
   for await (const line of rl) {
-    console.log(line);
+    lines.push(line);
   }
 
-  return 0;
+  var inputs: string[][] = [];
+  var outputs: string[][] = [];
+
+  for (const line of lines) {
+    var splitValue = line.split('|');
+    var input = splitValue[0].split(' ').filter(Boolean);
+    inputs.push(input);
+
+    var output = splitValue[1].split(' ').filter(Boolean);
+    outputs.push(output);
+  }
+
+  const result = await processInputs(inputs, outputs);
+
+  return result;
 }
 
 async function main() {
