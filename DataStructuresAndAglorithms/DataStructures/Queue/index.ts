4a0251f536f7f81a6950e7dc8aa0ae951e0f586e
Tom Bowyer <tom4310@gmail.com>
2022-01-16 10:37:43 +1000

    Qeue

--- DataStructuresAndAglorithms/DataStructures/Queue/index.ts
+++ DataStructuresAndAglorithms/DataStructures/Queue/index.ts
@@ -1,1 +1,38 @@
-console.log('Queue');
+export class Queue {
+	q: Array<any>;
+
+	constructor() {
+		this.q = new Array();
+	}
+
+	enqueue(element: any) {
+		this.q.push(element);
+	}
+
+	dequeue(): any {
+		return this.q.shift();
+	}
+
+	print() {
+		for (const v of this.q) {
+			console.log(v);
+		}
+	}
+}
+
+let q = new Queue();
+
+console.log("Add and print out queue")
+
+q.enqueue(5);
+q.enqueue(6);
+q.enqueue(7);
+q.enqueue(8);
+
+q.print();
+
+console.log("Dequeue")
+console.log(q.dequeue());
+
+console.log("print out current queue")
+q.print();
