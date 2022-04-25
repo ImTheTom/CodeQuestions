f5eb43c544a504a35963198ab4de05ecf279b0de
Tom Bowyer <tom4310@gmail.com>
2022-04-25 10:40:43 +1000

    Peeking Iterator

--- Leetcode/PeekingIterator/go.go
+++ Leetcode/PeekingIterator/go.go
@@ -2,12 +2,43 @@ package main
 
 import "fmt"
 
-func DoQuestion(first int, second int) int {
-	return first + second
+/*   Below is the interface for Iterator, which is already defined for you.
+ *
+ *   type Iterator struct {
+ *
+ *   }
+ *
+ *   func (this *Iterator) hasNext() bool {
+ *		// Returns true if the iteration has more elements.
+ *   }
+ *
+ *   func (this *Iterator) next() int {
+ *		// Returns the next element in the iteration.
+ *   }
+ */
+
+type PeekingIterator struct {
+	it *Iterator
+}
+
+func Constructor(iter *Iterator) *PeekingIterator {
+	return &PeekingIterator{
+		it: iter,
+	}
+}
+
+func (this *PeekingIterator) hasNext() bool {
+	return this.it.hasNext()
+}
+
+func (this *PeekingIterator) next() int {
+	return this.it.next()
+}
+
+func (this *PeekingIterator) peek() int {
+	return this.it.v[this.it.index]
 }
 
 func main() {
-	fmt.Println("Running main")
-	result := DoQuestion(1, 1)
-	fmt.Printf("Got %v\n", result)
+	fmt.Println("Need to run in leetcode since they implemented Iterator")
 }
