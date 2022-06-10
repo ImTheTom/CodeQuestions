89d0f5e3c711511c5e79a8f38b89a00249eb508e
Tom Bowyer <tom4310@gmail.com>
2022-06-11 08:22:26 +1000

    Add execution time in go sample

--- sample/go/go.go
+++ sample/go/go.go
@@ -1,13 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func DoQuestion(first int, second int) int {
 	return first + second
 }
 
 func main() {
+	start := time.Now()
+
 	fmt.Println("Running main")
 	result := DoQuestion(1, 1)
 	fmt.Printf("Got %v\n", result)
+
+	elapsed := time.Since(start)
+	fmt.Printf("Total execution time is %s\n", elapsed)
 }
