1338ae49bb19b8bce7cb7f6d1e51185aa1583c0d
Tom Bowyer <tom4310@gmail.com>
2022-06-18 09:19:24 +1000

    Do reverse linked list

--- Datastructures/linkedList/go.go
+++ Datastructures/linkedList/go.go
@@ -12,6 +12,13 @@ func main() {
 
 	fmt.Println("Running main")
 
+	DoReverseList()
+
+	elapsed := time.Since(start)
+	fmt.Printf("Total execution time is %s\n", elapsed)
+}
+
+func DoSingleList() {
 	l := &SingleLinkedList{}
 
 	l.Append(5)
@@ -41,7 +48,36 @@ func main() {
 
 	node = l.Search(39)
 	fmt.Println(node)
+}
 
-	elapsed := time.Since(start)
-	fmt.Printf("Total execution time is %s\n", elapsed)
+func DoReverseList() {
+	l := &ReverseLinkedList{}
+
+	l.Append(5)
+	l.Append(6)
+	l.Append(7)
+
+	l.String()
+
+	l.InsertToHead(2)
+
+	l.InsertAtIndex(3, 1)
+
+	l.String()
+
+	l.RemoveHead()
+	l.RemoveEnd()
+	l.RemoveAtIndex(2)
+
+	l.String()
+
+	l.Append(8)
+	l.Append(9)
+	l.Append(10)
+
+	node := l.Search(9)
+	fmt.Println(node)
+
+	node = l.Search(39)
+	fmt.Println(node)
 }
--- Datastructures/linkedList/reverse.go
+++ Datastructures/linkedList/reverse.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 type ReverseNode struct {
 	Val  int
 	Next *ReverseNode
@@ -8,4 +13,153 @@ type ReverseNode struct {
 
 type ReverseLinkedList struct {
 	Head *ReverseNode
+}
+
+func (l *ReverseLinkedList) Search(value int) *ReverseNode {
+	tmpNode := l.Head
+
+	for tmpNode != nil {
+		if tmpNode.Val == value {
+			return tmpNode
+		}
+
+		tmpNode = tmpNode.Next
+	}
+
+	return nil
+}
+
+func (l *ReverseLinkedList) Append(value int) {
+	newNode := &ReverseNode{
+		Val: value,
+	}
+
+	if l.Head == nil {
+		l.Head = newNode
+		return
+	}
+
+	tmpNode := l.Head
+	for tmpNode.Next != nil {
+		tmpNode = tmpNode.Next
+	}
+
+	tmpNode.Next = newNode
+	newNode.Prev = tmpNode
+}
+
+func (l *ReverseLinkedList) InsertToHead(value int) {
+	newNode := &ReverseNode{
+		Val:  value,
+		Next: l.Head,
+		Prev: nil,
+	}
+
+	l.Head.Prev = newNode
+	l.Head = newNode
+}
+
+func (l *ReverseLinkedList) InsertAtIndex(value, index int) error {
+	if index == 0 {
+		l.InsertToHead(value)
+		return nil
+	}
+
+	newNode := &ReverseNode{
+		Val: value,
+	}
+
+	tmpNode := l.Head
+	for i := 0; i < index-1; i++ {
+		if tmpNode == nil {
+			return errors.New("Attempting to insert at Index greater than length")
+		}
+		tmpNode = tmpNode.Next
+	}
+
+	tmpNodeNext := tmpNode.Next
+
+	tmpNode.Next = newNode
+
+	newNode.Next = tmpNodeNext
+	newNode.Prev = tmpNode
+
+	tmpNodeNext.Prev = newNode
+
+	return nil
+}
+
+func (l *ReverseLinkedList) RemoveHead() {
+	if l.Head == nil {
+		return
+	}
+
+	tmpNewHead := l.Head.Next
+
+	l.Head = nil
+
+	l.Head = tmpNewHead
+	l.Head.Prev = nil
+}
+
+func (l *ReverseLinkedList) RemoveEnd() {
+	if l.Head == nil {
+		return
+	}
+
+	tmpNode := l.Head
+	previousNode := l.Head
+
+	for tmpNode.Next != nil {
+		previousNode = tmpNode
+		tmpNode = tmpNode.Next
+	}
+
+	tmpNode = nil
+	previousNode.Next = nil
+}
+
+func (l *ReverseLinkedList) RemoveAtIndex(index int) error {
+	if index == 0 {
+		l.RemoveHead()
+		return nil
+	}
+
+	tmpNode := l.Head
+	previousNode := l.Head
+
+	for i := 0; i < index-1; i++ {
+		if tmpNode == nil {
+			return errors.New("Attempting to insert at Index greater than length")
+		}
+		previousNode = tmpNode
+		tmpNode = tmpNode.Next
+	}
+
+	previousNode.Next = tmpNode.Next
+	tmpNode.Next.Prev = tmpNode.Prev
+	tmpNode = nil
+
+	return nil
+}
+
+func (l *ReverseLinkedList) String() {
+	var (
+		index int
+		final string
+	)
+
+	tmpNode := l.Head
+
+	for tmpNode != nil {
+		if tmpNode.Prev != nil {
+			final = fmt.Sprintf("%sIndex - %d Value - %d Previous Value - %d\n", final, index, tmpNode.Val, tmpNode.Prev.Val)
+		} else {
+			final = fmt.Sprintf("%sIndex - %d Value - %d\n", final, index, tmpNode.Val)
+		}
+		index++
+		tmpNode = tmpNode.Next
+	}
+
+	fmt.Println(final)
 }
