9f548e83d143ac696981c600656bcac8cc489cde
Tom Bowyer <tom4310@gmail.com>
2022-06-19 08:30:45 +1000

    Tree

---
+++ Datastructures/tree/go.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type NodeWithParent struct {
+	Value  int
+	Left   *NodeWithParent
+	Right  *NodeWithParent
+	Parent *NodeWithParent
+}
+
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+type BinaryTree struct {
+	Root *Node
+}
+
+func (b *BinaryTree) Insert(value int) {
+	if b.Root == nil {
+		b.Root = &Node{
+			Value: value,
+		}
+		return
+	}
+
+	b.recursiveInsert(value, b.Root)
+}
+
+func (b *BinaryTree) recursiveInsert(value int, node *Node) {
+	// Should never get here, but good to have.
+	if node == nil {
+		return
+	}
+
+	// Should value go to the left?
+	if value <= node.Value {
+		if node.Left == nil {
+			node.Left = &Node{Value: value}
+
+			return
+		}
+
+		b.recursiveInsert(value, node.Left)
+
+		return
+	}
+
+	// Will continue here if going to the right
+	if node.Right == nil {
+		node.Right = &Node{Value: value}
+
+		return
+	}
+
+	b.recursiveInsert(value, node.Right)
+}
+
+func printPreOrder(n *Node) {
+	if n == nil {
+		return
+	} else {
+		fmt.Printf("%d ", n.Value)
+		printPreOrder(n.Left)
+		printPreOrder(n.Right)
+	}
+}
+
+func main() {
+	start := time.Now()
+
+	fmt.Println("Running main")
+
+	t := &BinaryTree{}
+
+	t.Insert(20)
+	t.Insert(10)
+	t.Insert(30)
+	t.Insert(40)
+	t.Insert(50)
+	t.Insert(5)
+	t.Insert(2)
+	t.Insert(7)
+	t.Insert(35)
+	t.Insert(45)
+	t.Insert(15)
+
+	printPreOrder(t.Root)
+
+	elapsed := time.Since(start)
+	fmt.Printf("Total execution time is %s\n", elapsed)
+}
