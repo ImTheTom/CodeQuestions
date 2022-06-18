f2295b502b3fd3a35ac6d6c392579ff85c42842e
Tom Bowyer <tom4310@gmail.com>
2022-06-18 16:30:17 +1000

    Qeue

---
+++ Datastructures/queue/go.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Queue struct {
+	Elements []int
+}
+
+func (q *Queue) Enqueue(value int) {
+	q.Elements = append(q.Elements, value)
+}
+
+func (q *Queue) Dequeue() int {
+	if len(q.Elements) == 0 {
+		return 0
+	}
+
+	element := q.Elements[0]
+
+	q.Elements = q.Elements[1:len(q.Elements)]
+
+	return element
+}
+
+func main() {
+	start := time.Now()
+
+	fmt.Println("Running main")
+
+	q := &Queue{}
+
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+
+	fmt.Println(q.Dequeue())
+	fmt.Println(q.Dequeue())
+	fmt.Println(q.Dequeue())
+	fmt.Println(q.Dequeue())
+
+	elapsed := time.Since(start)
+	fmt.Printf("Total execution time is %s\n", elapsed)
+}
